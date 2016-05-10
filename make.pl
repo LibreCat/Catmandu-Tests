8d3a14e89c4e55d0b9611068b02fb3f7a340fca3
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2016-05-10 14:43:16 +0200

    Fixing makefile

--- make.pl
+++ make.pl
@@ -12,51 +12,51 @@ my $cmd = shift;
 die "usage: $0 install|test|clean|realclean" unless $cmd;
 
 if ($cmd eq 'install') {
-	cmd_install();
+    cmd_install();
 }
 elsif ($cmd eq 'test') {
-	cmd_test();
+    cmd_test();
 }
 elsif ($cmd eq 'clean') {
-	cmd_clean();
+    cmd_clean();
 }
 elsif ($cmd eq 'realclean') {
-	cmd_realclean();
+    cmd_realclean();
 }
 
 sub cmd_install {
-	for my $pkg (@{$conf->{packages}}) {
-		my $name   = $pkg->{name};
-		my $git    = $pkg->{git};
-		my $branch = $pkg->{branch} // 'master';
-		system("./install_package.sh $name $git $branch");
-	}
+    for my $pkg (@{$conf->{packages}}) {
+        my $name   = $pkg->{name};
+        my $git    = $pkg->{git};
+        my $branch = $pkg->{branch} // 'master';
+        system("./install_package.sh $name $git $branch");
+    }
 }
 
 sub cmd_test {
-	for my $pkg (@{$conf->{packages}}) {
-		my $name   = $pkg->{name};
-		system("./test_package.sh $name");
-	}
+    for my $pkg (@{$conf->{packages}}) {
+        my $name   = $pkg->{name};
+        system("./test_package.sh $name");
+    }
 }
 
 sub cmd_clean {
-	for my $pkg (@{$conf->{packages}}) {
-		my $name   = $pkg->{name};
-		system("rm -rf $name/cpanfile.snapshot");
-	}
-	system("rm -rf local");
+    for my $pkg (@{$conf->{packages}}) {
+        my $name   = $pkg->{name};
+        system("rm -rf $name/cpanfile.snapshot");
+    }
+    system("rm -rf local");
 }
 
 sub cmd_realclean {
-	for my $pkg (@{$conf->{packages}}) {
-		my $name   = $pkg->{name};
-		system("rm -rf Catmandu*");
-	}
+    for my $pkg (@{$conf->{packages}}) {
+        my $name   = $pkg->{name};
+        system("rm -rf Catmandu*");
+    }
 }
 
 sub config {
-	local $/;
+    local $/;
     open( my $fh, '<', 'config.json' );
     my $json_text   = <$fh>;
     my $perl_scalar = decode_json( $json_text );
